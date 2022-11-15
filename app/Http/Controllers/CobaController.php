0309fe6018b453287b443619ef1f8d9b82054259
fillajunior <pantailovina@gmail.com>
2022-11-15 19:05:56 +0800

    fix bug and add export

--- app/Http/Controllers/CobaController.php
+++ app/Http/Controllers/CobaController.php
@@ -9,7 +9,7 @@ class CobaController extends Controller
 {
     public function coba()
     {
-        dd(Crypt::decrypt('eyJpdiI6IkNmN29wMmh0L1JFRHdaZUlNM1ZwNEE9PSIsInZhbHVlIjoieXI5WHFYd05hTWFMVEtUSFUwU0JuUT09IiwibWFjIjoiYjc4OTk5MDJlZDQyYjc3NjNkYjIzM2FjODMwNDlkMDVmMjJmZTVlODRlN2Q4YzcxYjU5MGQ3OWU3OTFjNjAwNCIsInRhZyI6IiJ9'));
+        // dd(Crypt::decrypt('eyJpdiI6IkNmN29wMmh0L1JFRHdaZUlNM1ZwNEE9PSIsInZhbHVlIjoieXI5WHFYd05hTWFMVEtUSFUwU0JuUT09IiwibWFjIjoiYjc4OTk5MDJlZDQyYjc3NjNkYjIzM2FjODMwNDlkMDVmMjJmZTVlODRlN2Q4YzcxYjU5MGQ3OWU3OTFjNjAwNCIsInRhZyI6IiJ9'));
         // $bln = date('m') - 6;
         // if ($bln < 0) {
         //     $bln2 = date('y', strtotime('+' . abs($bln) .'month', strtotime('+1 year', strtotime('-3 year'))));
