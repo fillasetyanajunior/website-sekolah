95ebdb8e1e16323b1ae43104f4c53bb51f686a3c
fillajunior <pantailovina@gmail.com>
2022-11-15 19:00:03 +0800

    fix bug and add export

--- app/Http/Controllers/CobaController.php
+++ app/Http/Controllers/CobaController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class CobaController extends Controller
 {
     public function coba()
     {
-        $bln = date('m') - 6;
-        if ($bln < 0) {
-            $bln2 = date('y', strtotime('+' . abs($bln) .'month', strtotime('+1 year', strtotime('-3 year'))));
-        }else{
-            $bln2 = date('y', strtotime('-' . abs($bln) . 'month', strtotime('+1 year', strtotime('-3 year'))));
-        }
-        dd($bln2);
+        dd(Crypt::decrypt('eyJpdiI6IkNmN29wMmh0L1JFRHdaZUlNM1ZwNEE9PSIsInZhbHVlIjoieXI5WHFYd05hTWFMVEtUSFUwU0JuUT09IiwibWFjIjoiYjc4OTk5MDJlZDQyYjc3NjNkYjIzM2FjODMwNDlkMDVmMjJmZTVlODRlN2Q4YzcxYjU5MGQ3OWU3OTFjNjAwNCIsInRhZyI6IiJ9'));
+        // $bln = date('m') - 6;
+        // if ($bln < 0) {
+        //     $bln2 = date('y', strtotime('+' . abs($bln) .'month', strtotime('+1 year', strtotime('-3 year'))));
+        // }else{
+        //     $bln2 = date('y', strtotime('-' . abs($bln) . 'month', strtotime('+1 year', strtotime('-3 year'))));
+        // }
+        // dd($bln2);
     }
 }
